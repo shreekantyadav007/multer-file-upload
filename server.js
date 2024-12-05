b86437bd1a3cb47b5995701e97da91c6650dcf6f
Shreekant Shreeram Yadav <shreekantyadav007@gmail.com>
2024-12-05 15:44:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(null,Date.now()+"-"+file.originalname);
   },
 });
 const upload = multer({ storage: storage });
